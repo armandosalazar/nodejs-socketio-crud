e9786164602913b26dc395e7fd6f27ef15b4a687
Armando Salazar <armandosalazar351@gmail.com>
2023-07-17 23:46:37 -0600

    refactor: separate files

--- src/public/main.js
+++ src/public/main.js
@@ -1,8 +1,5 @@
-socket = io(); // io('http://localhost:4001');
-
 const form = document.querySelector('#form-notes');
 const notification = document.querySelector('#notification');
-const notes = document.querySelector('#container-notes');
 
 form.addEventListener('submit', (event) => {
     event.preventDefault();
@@ -14,25 +11,5 @@ form.addEventListener('submit', (event) => {
 
     const note = { title, description };
 
-    socket.emit('client:[new-note]', note);
-});
-
-socket.on('server:[new-note]', (note) => {
-    console.log(note);
-
-    const noteElement = document.createElement('article');
-    noteElement.classList.add('note');
-    noteElement.innerHTML = `
-        <h3>${note.title}</h3>
-        <p>${note.description}</p>
-    `;
-
-    notes.appendChild(noteElement);
-
-
-    notification.innerHTML = `<p>New note!</p>`;
-    notification.style = 'display: block;';
-    setTimeout(() => {
-        notification.style = 'display: none;';
-    }, 1000);
+    saveNote(note);
 });
---
+++ src/public/ui.js
@@ -0,0 +1,18 @@
+const notes = document.querySelector('#container-notes');
+
+function appendNote(note) {
+    const noteElement = document.createElement('article');
+    noteElement.classList.add('note');
+    noteElement.innerHTML = `
+        <h3>${note.title}</h3>
+        <p>${note.description}</p>
+    `;
+
+    notes.appendChild(noteElement);
+
+    notification.innerHTML = `<p>New note!</p>`;
+    notification.style = 'display: block;';
+    setTimeout(() => {
+        notification.style = 'display: none;';
+    }, 1000);
+}
