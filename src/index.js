f2b19c0ec8b1f766a3a75e5e9140dbd3dbf711c8
Armando Salazar <armandosalazar351@gmail.com>
2023-07-17 20:55:38 -0600

    feat: add socket.io connection

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import express from 'express';
+import { Server } from 'socket.io';
+import http from 'http';
 
 const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
 
 app.use(express.static(__dirname + '/public'));
 
-app.listen(4001);
-console.log('http://localhost:4001');
+io.on('connection', (socket) => {
+	console.log('a user connected');
+	console.log('info:', socket);
+});
+
+server.listen(4001, () => {
+	console.log('http://localhost:4001');
+});
---
+++ src/public/main.js
@@ -0,0 +1,3 @@
+console.log('Hello World from main.js!');
+// io('http://localhost:4001')
+io = io();
