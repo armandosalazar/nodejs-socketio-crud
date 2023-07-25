f57894f1fdb9e6923f5b2ab78a73eb97703f14b9
Armando Salazar <armandosalazar351@gmail.com>
2023-07-25 14:35:39 -0600

    feat: ready for deploy

--- src/index.js
+++ src/index.js
@@ -11,4 +11,4 @@ const server = http.createServer(app);
 sockets(new Server(server));
 
 server.listen(PORT);
-console.log('Server running on: http://localhost:${PORT}'); // dev
+console.log(`Server running on: http://localhost:${PORT}`); // dev
